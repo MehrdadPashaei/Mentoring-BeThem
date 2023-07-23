5ab7c1c626aea993f42143d885a2f0f7465b0906
Mehrdad.Pashaei <mehrdad.pashaei1@gmail.com>
2023-07-23 01:06:27 -0700

    feat(new repository) : creat a new repository for my team.

---
+++ assets/home/javascript/swiper.js
@@ -0,0 +1,11 @@
+
+let slide1 = document.querySelector(".slide1");
+let slide2 = document.querySelector(".slide2");
+let slide3 = document.querySelector(".slide3");
+let next = document.querySelector(".next");
+let prev = document.querySelector(".prev");
+
+function slider () {
+
+}
+slider()
